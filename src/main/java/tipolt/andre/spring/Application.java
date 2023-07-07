7e22485ed41bb8292a2915653f6325e234416349
andretipolt <andretipoltlopes@gmail.com>
2023-07-07 15:52:55 -0300

    Realising h2-conle url

--- src/main/java/tipolt/andre/spring/Application.java
+++ src/main/java/tipolt/andre/spring/Application.java
@@ -2,13 +2,11 @@ package tipolt.andre.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println(new BCryptPasswordEncoder().encode("luana1234"));
 	}
 }
--- src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
+++ src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package tipolt.andre.spring.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,6 +19,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private TokenStore tokenStore;
 
+    @Autowired
+    private Environment env;
+
     private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
@@ -29,6 +35,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
