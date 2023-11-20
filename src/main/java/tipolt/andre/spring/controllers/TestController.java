322bfe007fafe6d76849199843a359445da03844
andretipolt <andretipoltlopes@gmail.com>
2023-11-20 14:36:59 -0300

    Reading list in redis keys

--- src/main/java/tipolt/andre/spring/controllers/TestController.java
+++ src/main/java/tipolt/andre/spring/controllers/TestController.java
@@ -1,6 +1,7 @@
 package tipolt.andre.spring.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import tipolt.andre.spring.models.ProductModel;
 import tipolt.andre.spring.services.ProductService;
 import tipolt.andre.spring.services.RedisCacheService;
 
@@ -34,18 +37,18 @@ public class TestController implements Serializable{
         ObjectMapper objectMapper = new ObjectMapper();
 
         String json = objectMapper.writeValueAsString(productService.findAll());
-        redisCacheService.saveJson("product", "lalalalala");
+        redisCacheService.saveJson("product", json);
     }
 
     @GetMapping("/get")
-    public String getJson() throws JsonMappingException, JsonProcessingException {
+    public List<ProductModel> getJson() throws JsonMappingException, JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         
         String response = redisCacheService.getJson("product");
 
-        String responseRight = objectMapper.readValue(response, String.class);
+        List<ProductModel> jsonNode = objectMapper.readValue(response, new TypeReference<List<ProductModel>>() {});
 
-        return responseRight;
+        return jsonNode;
     }
 }
