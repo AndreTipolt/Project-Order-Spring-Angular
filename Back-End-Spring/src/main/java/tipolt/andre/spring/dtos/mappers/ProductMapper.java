7718c15f17f056ad2e1f8614af4f8e7ae1793e6a
andretipolt <andretipoltlopes@gmail.com>
2024-01-04 18:24:30 -0300

    Create Product Mapper

---
+++ Back-End-Spring/src/main/java/tipolt/andre/spring/dtos/mappers/ProductMapper.java
@@ -0,0 +1,47 @@
+package tipolt.andre.spring.dtos.mappers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import tipolt.andre.spring.dtos.ProductDTO;
+import tipolt.andre.spring.models.CategoryModel;
+import tipolt.andre.spring.models.ProductModel;
+import tipolt.andre.spring.repositories.CategoryRepository;
+
+@Component
+public class ProductMapper {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public ProductDTO convertToProductDTO(ProductModel productModel) {
+
+        ProductDTO productDTO = new ProductDTO();
+
+        productDTO.setInstallments(productModel.getInstallments());
+        productDTO.setName(productModel.getName());
+        productDTO.setPixDiscount(productModel.getPixDiscount());
+        productDTO.setSpotPrice(productModel.getSpotPrice());
+        productDTO.setDescription(productModel.getDescription());
+        productDTO.setCategoryId(productModel.getCategory().getId());
+        productDTO.setInstallments(productModel.getInstallments());
+        return productDTO;
+    }
+
+    public ProductModel convertToProductModel(ProductDTO productDTO) {
+
+        ProductModel productModel = new ProductModel();
+
+        productModel.setName(productDTO.getName());
+        productModel.setFowardPrice(productDTO.getFowardPrice());
+        productModel.setSpotPrice(productDTO.getSpotPrice());
+        productModel.setDescription(productDTO.getDescription());
+        productModel.setInstallments(productDTO.getInstallments());
+        productModel.setPixDiscount(productDTO.getPixDiscount());
+
+        CategoryModel category = categoryRepository.findById(productDTO.getCategoryId()).get();
+        productModel.setCategory(category);
+
+        return productModel;
+    }
+}
