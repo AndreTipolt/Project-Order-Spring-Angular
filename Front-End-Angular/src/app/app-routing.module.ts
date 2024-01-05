300b9eac38d80f91acbfcd1a2cf524808ecf9dcb
andretipolt <andretipoltlopes@gmail.com>
2024-01-05 13:56:29 -0300

    Create route /login and module auth

--- Front-End-Angular/src/app/app-routing.module.ts
+++ Front-End-Angular/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListProductsComponent } from './products/components/list-products/list-products.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
