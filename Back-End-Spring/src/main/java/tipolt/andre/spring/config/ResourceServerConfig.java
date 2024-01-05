5dac7b71e39ce2bc4da3ed49e814030d19f6d00b
andretipolt <andretipoltlopes@gmail.com>
2024-01-05 19:26:49 -0300

    Configurating CORS

--- Back-End-Spring/src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
+++ Back-End-Spring/src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
@@ -3,14 +3,20 @@ package tipolt.andre.spring.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -22,11 +28,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private Environment env;
 
-    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/products/**", "/users/save" };
+    // private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/products/**", "/users/save" };
 
-    private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**", "/myorders/**" };
+    // private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**", "/myorders/**" };
 
-    private static final String[] ADMIN = { "/users/**", "/orders/**" };
+    // private static final String[] ADMIN = { "/users/**", "/orders/**" };
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -39,13 +45,39 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         if(Arrays.asList(env.getActiveProfiles()).contains("test")){
             http.headers().frameOptions().disable();
         }
-        http.authorizeRequests()
-            .anyRequest().permitAll();
-    //             .antMatchers(PUBLIC).permitAll()
-    //             .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
-    //             .antMatchers(OPERATOR_OR_ADMIN).hasAnyAuthority("OPERATOR", "ADMIN")
-    //             .antMatchers(ADMIN).hasAuthority("ADMIN")
-    //             .anyRequest().authenticated();
+        http.authorizeRequests(requests -> requests
+                .anyRequest().permitAll());
+        //             .antMatchers(PUBLIC).permitAll()
+        //             .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
+        //             .antMatchers(OPERATOR_OR_ADMIN).hasAnyAuthority("OPERATOR", "ADMIN")
+        //             .antMatchers(ADMIN).hasAuthority("ADMIN")
+        //             .anyRequest().authenticated();
+
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+        corsConfig.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 }
