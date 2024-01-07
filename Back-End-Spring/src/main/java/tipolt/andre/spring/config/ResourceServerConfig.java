c1fc858c9af5a9f12355f45806dabae0cce25757
andretipolt <andretipoltlopes@gmail.com>
2024-01-07 01:10:29 -0300

    Create a new form to implements a login, beggining removing the old method

--- Back-End-Spring/src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
+++ Back-End-Spring/src/main/java/tipolt/andre/spring/config/ResourceServerConfig.java
@@ -2,62 +2,48 @@ package tipolt.andre.spring.config;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig {
 
-    @Autowired
-    private TokenStore tokenStore;
+    // private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**",
+    // "/products/**", "/users/save" };
 
-    @Autowired
-    private Environment env;
-
-    // private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/products/**", "/users/save" };
-
-    // private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**", "/myorders/**" };
+    // private static final String[] OPERATOR_OR_ADMIN = { "/products/**",
+    // "/categories/**", "/myorders/**" };
 
     // private static final String[] ADMIN = { "/users/**", "/orders/**" };
 
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenStore(tokenStore);
-    }
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    // if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+    // http.headers().frameOptions().disable();
+    // }
+    http.authorizeRequests(requests -> requests.anyRequest().permitAll());
+    // .antMatchers(PUBLIC).permitAll()
+    // .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
+    // .antMatchers(OPERATOR_OR_ADMIN).hasAnyAuthority("OPERATOR", "ADMIN")
+    // .antMatchers(ADMIN).hasAuthority("ADMIN")
+    // .anyRequest().authenticated();
 
-        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
-            http.headers().frameOptions().disable();
-        }
-        http.authorizeRequests(requests -> requests
-                .anyRequest().permitAll());
-        //             .antMatchers(PUBLIC).permitAll()
-        //             .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).authenticated()
-        //             .antMatchers(OPERATOR_OR_ADMIN).hasAnyAuthority("OPERATOR", "ADMIN")
-        //             .antMatchers(ADMIN).hasAuthority("ADMIN")
-        //             .anyRequest().authenticated();
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
+    public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration corsConfig = new CorsConfiguration();
 
@@ -72,9 +58,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter(){
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
 
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(corsConfigurationSource()));
 
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
