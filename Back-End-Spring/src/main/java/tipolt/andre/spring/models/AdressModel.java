3c71f7728481b45b373af696408e50f0781ac906
andretipolt <andretipoltlopes@gmail.com>
2024-01-15 19:20:43 -0300

    Create entity adress

---
+++ Back-End-Spring/src/main/java/tipolt/andre/spring/models/AdressModel.java
@@ -0,0 +1,53 @@
+package tipolt.andre.spring.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import tipolt.andre.spring.models.enums.TypeAdressEnum;
+import tipolt.andre.spring.models.enums.converters.TypeAdressEnumConverter;
+
+@Entity
+@Table(name = "tb_adresses")
+@Data
+public class AdressModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String street;
+    
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String neighborhood;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = true)
+    private String complement;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Convert(converter = TypeAdressEnumConverter.class)
+    private TypeAdressEnum typeAdress;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserModel user;
+}
