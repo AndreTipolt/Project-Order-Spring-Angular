eb3abc976b09b7d330d8ff2e99c33c7fe596a0c8
andretipolt <andretipoltlopes@gmail.com>
2024-01-24 23:31:44 -0300

    Fixing the height at show products in cart and add the resolver auth in show cart

--- Front-End-Angular/src/app/cart/cart-routing.module.ts
+++ Front-End-Angular/src/app/cart/cart-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShowCartComponent } from './components/show-cart/show-cart.component';
+import { AuthResolver } from '../auth/guards/auth.resolver';
 
 const routes: Routes = [
-  { path: '', component: ShowCartComponent }
+  { path: '', component: ShowCartComponent, resolve:[AuthResolver] }
 ];
 
 @NgModule({
